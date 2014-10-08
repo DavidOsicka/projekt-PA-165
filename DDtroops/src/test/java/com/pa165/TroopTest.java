157331874aa6ea08626a59a2d01d75263516b63d
sotek13 <sotek13@seznam.cz>
2014-10-08 18:52:35 +0200

    TroopTest implementation
    created tests for troop dao class

--- DDtroops/src/test/java/com/pa165/TroopTest.java
+++ DDtroops/src/test/java/com/pa165/TroopTest.java
@@ -6,9 +6,15 @@
 package com.pa165;
 
 
+import com.pa165.ddtroops.dao.TroopDAO;
 import com.pa165.ddtroops.daoimpl.TroopDAOImpl;
 import com.pa165.ddtroops.entity.Troop;
-import org.junit.Assert;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import org.testng.Assert;
+import static org.testng.Assert.*;
+import org.testng.annotations.Test;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -17,11 +23,14 @@ import org.testng.annotations.Test;
 
 /**
  *
- * @author Jakub
+ * @author Jakub Szotkowski
  */
 public class TroopTest {
     
+    private static TroopDAO troopDao;
+    
     public TroopTest() {
+        troopDao = new TroopDAOImpl();
     }
 
     // TODO add test methods here.
@@ -46,15 +55,75 @@ public class TroopTest {
     public void tearDownMethod() throws Exception {
     }
     
+    private Troop createStartTroop() {
+        Troop troop = new Troop();
+        troop.setName("Killers_" + new Random().nextInt());
+        troop.setMission("Go to White Castle and kill Big Yeti");
+        troop.setAmountOfGM(150);
+        return troop;
+    }
+    
+    private Troop createOneTroop() {
+        Troop t = createStartTroop();
+        troopDao.createTroop(t);
+        return t;
+    }
+    
     @Test
-    public void createNewTroop(){
-	Troop t = new Troop();
-        t.setName("Killers");
-        t.setMission("Go to White Castle and kill Big Yeti");
-        t.setAmountOfGM(150);
-        TroopDAOImpl tDAO = new TroopDAOImpl();
-        tDAO.createTroop(t);
-        Assert.assertEquals(t.getId(), 1);
-        Assert.assertEquals(t.getName(), "Killers");
+    public void createTroopTest(){
+	Troop t = createOneTroop();
+        Assert.assertEquals(troopDao.retrieveTroopById(t.getId()), t, "Expected troop is not equal.");
+    }
+    
+    @Test
+    public void updateTroopTest(){
+        Troop t = createOneTroop();
+        t.setMission("Find Little pony and help him kill wicked witch");
+        t.setAmountOfGM(300);
+        troopDao.updateTroop(t);
+        Troop dbTroop = troopDao.retrieveTroopById(t.getId());
+        Assert.assertEquals(dbTroop.getMission(), "Find Little pony and help him kill wicked witch", "Mission is not equal.");
+        Assert.assertEquals(dbTroop.getAmountOfGM(), 300, "Golden money is not equal.");
+    }
+    
+    @Test
+    public void deleteTroopTest(){
+        Troop t = createOneTroop();
+        troopDao.deleteTroop(t);
+        Troop dbTroop = troopDao.retrieveTroopById(t.getId());
+        Assert.assertNull(dbTroop, "Troop wasn´t deleted.");
+    }
+    
+    @Test
+    public void retrieveAllTroopsTest(){
+        List<Troop> allTroops = troopDao.retrieveAllTroops();
+        if(allTroops.size()>0) {
+            for (Troop allTroop : allTroops) {
+                troopDao.deleteTroop(allTroop);
+            }
+        }
+        allTroops = troopDao.retrieveAllTroops();
+        Assert.assertEquals(allTroops.size(), 0);
+        for(int i=0; i<5; i++){
+            createOneTroop();
+        }
+        allTroops = troopDao.retrieveAllTroops();
+        Assert.assertEquals(allTroops.size(), 5);
+    }
+    
+    @Test
+    public void retrieveTroopByIdTest() {
+        Troop t = createOneTroop();
+        Troop dbTroop = troopDao.retrieveTroopById(t.getId());
+        Assert.assertNotNull(dbTroop);
+    }
+    
+    @Test
+    public void retrieveTroopByNameTest() {
+        Troop t = createStartTroop();
+        t.setName("Croosters");
+        troopDao.createTroop(t);
+        Troop dbTroop = troopDao.retrieveTroopByName("Croosters");
+        Assert.assertNotNull(dbTroop);
     }
 }
