f677da793db6d128d95d4e0ebe47eefb2b3c672b
peskamartin <mates.13@email.cz>
2014-10-08 13:54:13 +0200

    Role implementation
    Implemented Role.java, RoleDAOImpl.java, created AdminTest.java
    missing implementation of tests and JavaDoc

--- DDtroops/src/main/java/com/pa165/ddtroops/daoimpl/RoleDAOImpl.java
+++ DDtroops/src/main/java/com/pa165/ddtroops/daoimpl/RoleDAOImpl.java
@@ -4,35 +4,84 @@ package com.pa165.ddtroops.daoimpl;
 import com.pa165.ddtroops.dao.RoleDAO;
 import com.pa165.ddtroops.entity.Role;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.PersistenceUnit;
+import org.springframework.test.context.ContextConfiguration;
 
-
+@ContextConfiguration(classes=DaoContext.class)
 public class RoleDAOImpl implements RoleDAO {
 
+    @PersistenceUnit
+    public EntityManagerFactory emf = Persistence.createEntityManagerFactory("myUnit");
+    
     public RoleDAOImpl () {
     }
 
+    @Override
     public Role createRole (Role role) {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(role);
+        em.getTransaction().commit();
+        em.close();
+        return role;
     }
 
+    @Override
     public Role updateRole (Role role) {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        Role updateRole = em.find(Role.class, role.getId());
+        em.getTransaction().begin();
+        updateRole.setName(role.getName());
+        updateRole.setDescription(role.getDescription());
+        updateRole.setEnergy(role.getEnergy());
+        updateRole.setAttack(role.getAttack());
+        updateRole.setDefense(role.getDefense());
+        updateRole.setHeroes(role.getHeroes());
+        em.getTransaction().commit();
+        em.close();
+        return updateRole;
     }
 
+    @Override
     public Boolean deleteRole (Role role) {
-        return null;
+        try {
+            EntityManager em = emf.createEntityManager();
+            Role deleteRole = em.find(Role.class, role.getId());
+            em.getTransaction().begin();
+            em.remove(deleteRole);
+            em.getTransaction().commit();
+            em.close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
+    @Override
     public List<Role> retrieveAllRoles () {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        List<Role> allRoles = em.createQuery("SELECT r FROM Role r", Role.class).getResultList();
+	em.close();
+        return allRoles;
     }
 
+    @Override
     public Role retrieveRoleById (long id) {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        Role roleById = em.find(Role.class, id);
+        em.close();
+        return roleById;
     }
 
+    @Override
     public Role retrieveRoleByName (String name) {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        Role roleByName = em.createQuery("SELECT r FROM Role r WHERE name=:name", Role.class).setParameter("name", name).getSingleResult();
+        em.close();
+        return roleByName;
     }
 
 }
--- DDtroops/src/main/java/com/pa165/ddtroops/entity/Role.java
+++ DDtroops/src/main/java/com/pa165/ddtroops/entity/Role.java
@@ -1,20 +1,35 @@
 package com.pa165.ddtroops.entity;
 
 import java.util.Set;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+/** 
+ * @author Martin Peška
+ * 
+ * Represents role entity in D&D troops. Hero has following attributes: name, 
+ * description, energy, attack, defense and  set of heroes with the role.
+ */
 @Entity
 public class Role {
     @Id
     @GeneratedValue
     private long id;
-
+    
+    @Column(nullable = false, unique = true)
     private String name;
 
+    @Column(nullable = false)
     private String description;
+    
+    private int energy;
+    
+    private int attack;
+    
+    private int defense;
     
     @ManyToMany
     private Set<Hero> heroes;
@@ -53,6 +68,54 @@ public class Role {
     public void setName (String val) {
         this.name = val;
     }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Role other = (Role) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 
 }
 
