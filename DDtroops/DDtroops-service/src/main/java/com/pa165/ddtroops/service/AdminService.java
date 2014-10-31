8c62297a113dbe0ef1d7dc242416f5c5c59657ef
Martin Jelinek <mjelinek@barinka.net>
2014-10-31 16:41:21 +0100

    AdminService with all methods that are in AdminDAO

--- DDtroops/DDtroops-service/src/main/java/com/pa165/ddtroops/service/AdminService.java
+++ DDtroops/DDtroops-service/src/main/java/com/pa165/ddtroops/service/AdminService.java
@@ -1,18 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.pa165.ddtroops.service;
 
 import com.pa165.ddtroops.dto.AdminDTO;
+import java.util.List;
 
 /**
  *
  * @author Martin Jelínek
+ * 
+ * Interface for D&D admin service
  */
 public interface AdminService {
     
+    /**
+     * Insert new admin into database.
+     * 
+     * @param admin admin to be inserted
+     * @return newly created admin
+     */
     public AdminDTO createAdmin(AdminDTO admin);
+
+    /**
+     * Update admin, that has already been persisted, in database.
+     * 
+     * @param admin admin to be updated
+     * @return updated admin
+     */
+    public AdminDTO updateAdmin(AdminDTO admin);
+
+    /**
+     * Delete admin from database.
+     * 
+     * @param admin admin to be deleted
+     * @return true if admin is deleted, false otherwise
+     */
+    public Boolean deleteAdmin(AdminDTO admin);
+
+    /**
+     * Retrieve list of all admins from database.
+     * 
+     * @return list of admins
+     */
+    public List<AdminDTO> retrieveAllAdmins();
+
+    /**
+     * Retrieve one admin with unique id.
+     * 
+     * @param id admin id
+     * @return admin with given id or null if admin doesn't exist
+     */
+    public AdminDTO retrieveAdminById(long id);
+
+    /**
+     * Retrieve one admin with unique name.
+     * @param name admin name
+     * @return admin with given name or null if admin doesn't exist
+     */
+    public AdminDTO retrieveAdminByName(String name);
+    
+    /**
+     * Delete all admins from database.
+     * 
+     * @return true if all admins were deleted, false otherwise
+     */
+    public Boolean deleteAllAdmins();
     
 }
--- DDtroops/DDtroops-service/src/main/java/com/pa165/ddtroops/serviceimpl/AdminServiceImpl.java
+++ DDtroops/DDtroops-service/src/main/java/com/pa165/ddtroops/serviceimpl/AdminServiceImpl.java
@@ -9,6 +9,8 @@ import com.pa165.ddtroops.dao.AdminDAO;
 import com.pa165.ddtroops.dto.AdminDTO;
 import com.pa165.ddtroops.entity.Admin;
 import com.pa165.ddtroops.service.AdminService;
+import java.util.ArrayList;
+import java.util.List;
 import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,10 +29,60 @@ public class AdminServiceImpl implements AdminService {
     
     @Autowired
     private AdminDAO adminDAO;
+    
+    private AdminDTO mapDTO(Admin admin) {
+        return mapper == null ? null : mapper.map(admin, AdminDTO.class);
+    }
+    
+    private Admin mapEntity(AdminDTO adminDTO) {
+        return mapper == null ? null : mapper.map(adminDTO, Admin.class);
+    }
 
     @Override
+    public Boolean deleteAllAdmins() {
+        Boolean result = true;
+        for (AdminDTO admin : retrieveAllAdmins()) {
+            result &= deleteAdmin(admin);
+        }
+        return result;
+    }
+
+    // methods same as in DAO
+    @Override
     public AdminDTO createAdmin(AdminDTO admin) {
-        return mapper.map(adminDAO.createAdmin(mapper.map(admin, Admin.class)), AdminDTO.class);
+        return mapDTO(adminDAO.createAdmin(
+                mapEntity(admin)));
+    }
+
+    @Override
+    public AdminDTO updateAdmin(AdminDTO admin) {
+        return mapDTO(adminDAO.updateAdmin(
+                mapEntity(admin)));
+    }
+
+    @Override
+    public Boolean deleteAdmin(AdminDTO admin) {
+        return adminDAO.deleteAdmin(
+                mapEntity(admin));
+    }
+
+    @Override
+    public List<AdminDTO> retrieveAllAdmins() {
+        List<AdminDTO> admins = new ArrayList<>();
+        for (Admin admin : adminDAO.retrieveAllAdmins()) {
+            admins.add(mapDTO(admin));
+        }
+        return admins;
+    }
+
+    @Override
+    public AdminDTO retrieveAdminById(long id) {
+        return mapDTO(adminDAO.retrieveAdminById(id));
+    }
+
+    @Override
+    public AdminDTO retrieveAdminByName(String name) {
+        return mapDTO(adminDAO.retrieveAdminByName(name));
     }
     
 }
