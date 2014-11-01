8355abab51b7e1b3ae7de9e25ea92cc9cee14826
sotek13 <sotek13@seznam.cz>
2014-11-01 17:17:01 +0100

    add Hero service

---
+++ DDtroops/DDtroops-service/src/main/java/com/pa165/ddtroops/service/HeroService.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pa165.ddtroops.service;
+
+import com.pa165.ddtroops.dto.HeroDTO;
+import java.util.List;
+
+/**
+ *
+ * @author Jakub Szotkowski
+ * 
+ * Interface for Hero service
+ */
+public interface HeroService {
+    
+    /**
+     * Creates new hero in database and return him.
+     * 
+     * @param hero  Hero, what we want save to database without id
+     * @return  hero with id from database
+     */
+    public HeroDTO createHero (HeroDTO hero);
+    
+    /**
+     * Update hero in database and return updated hero.
+     * @param hero  hero need to update
+     * @return  updated hero
+     */
+    public HeroDTO updateHero (HeroDTO hero);
+
+    /**
+     * Delete hero from database.
+     * @param hero hero, what we want delete from database
+     * @return  true if hero is deleted, false otherwise
+     */
+    public Boolean deleteHero (HeroDTO hero);
+
+    /**
+     * Retrieve list of all heroes from database.
+     * @return  list of heroes
+     */
+    public List<HeroDTO> retrieveAllHeroes ();
+
+    /**
+     * Retrieve one hero with unique id.
+     * @param id    hero id
+     * @return  hero with choosen id
+     */
+    public HeroDTO retrieveHeroById (long id);
+
+    /**
+     * Retrieve one hero with unique name.
+     * @param name  hero name
+     * @return  hero with choosen name
+     */
+    public HeroDTO retrieveHeroByName (String name);
+    
+    /**
+     * Delete all heroes from database.
+     * 
+     * @return true if all heroes were deleted, false otherwise 
+     */
+    public Boolean deleteAllHeroes();
+    
+}
---
+++ DDtroops/DDtroops-service/src/main/java/com/pa165/ddtroops/serviceimpl/HeroServiceImpl.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pa165.ddtroops.serviceimpl;
+
+import com.pa165.ddtroops.dao.HeroDAO;
+import com.pa165.ddtroops.dto.HeroDTO;
+import com.pa165.ddtroops.entity.Hero;
+import com.pa165.ddtroops.service.HeroService;
+import java.util.ArrayList;
+import java.util.List;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author Jakub Szotkowski
+ * 
+ * Implementation of Hero service interface
+ */
+@Service
+@Transactional
+public class HeroServiceImpl implements HeroService{
+
+    @Autowired
+    private Mapper mapper;
+    
+    @Autowired
+    private HeroDAO heroDao;
+    
+    private HeroDTO mapDTO(Hero hero) {
+        return mapper == null ? null : mapper.map(hero, HeroDTO.class);
+    }
+    
+    private Hero mapEntity(HeroDTO heroDTO) {
+        return mapper == null ? null : mapper.map(heroDTO, Hero.class);
+    }
+    
+    @Override
+    public HeroDTO createHero(HeroDTO hero) {
+        return mapDTO(heroDao.createHero(mapEntity(hero)));
+    }
+
+    @Override
+    public HeroDTO updateHero(HeroDTO hero) {
+        return mapDTO(heroDao.updateHero(mapEntity(hero)));
+    }
+
+    @Override
+    public Boolean deleteHero(HeroDTO hero) {
+        return heroDao.deleteHero(mapEntity(hero));
+    }
+
+    @Override
+    public List<HeroDTO> retrieveAllHeroes() {
+        List<HeroDTO> allHeroes = new ArrayList();
+        for(Hero h : heroDao.retrieveAllHeroes()) {
+            allHeroes.add(mapDTO(h));
+        }
+        return allHeroes;
+    }
+
+    @Override
+    public HeroDTO retrieveHeroById(long id) {
+        return mapDTO(heroDao.retrieveHeroById(id));
+    }
+
+    @Override
+    public HeroDTO retrieveHeroByName(String name) {
+        return mapDTO(heroDao.retrieveHeroByName(name));
+    }
+
+    @Override
+    public Boolean deleteAllHeroes() {
+        Boolean res = true;
+        for(HeroDTO h : retrieveAllHeroes()) {
+            res &= deleteHero(h);
+        }
+        return res;
+    }
+    
+}
